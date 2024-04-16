625abca2b5921dcb7b119b31bb34c3b710ec644e
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-04-16 07:16:57 +0300

    First iteration

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -110,7 +110,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
